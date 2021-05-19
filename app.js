25a569e979201db483a61d1308e66bae94ddb125
Dustin Cartwright <therandomdust@gmail.com>
2021-05-19 05:21:57 -0700

    Added Joi for server-side validation in a few of the routes middleware

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const Hometown = require('./models/hometown');
 const ejsMate = require('ejs-mate');
 const methodOverride = require('method-override');
 const morgan = require('morgan'); // TODO maybe remove later
+const Joi = require('joi');
 const AppError = require('./utils/AppError');
 const wrapAsync = require('./utils/wrapAsync');
 
@@ -34,6 +35,25 @@ app.use(express.urlencoded({ extended: true })); // middleware to be able to par
 app.use(methodOverride('_method')); // to use query param to override forms to delete / put / patch
 app.use(morgan('dev')); // TODO maybe remove later
 
+const validateHometown = (req, res, next) => {
+	const hometownSchema = Joi.object({
+		hometown: Joi.object({
+			town: Joi.string().required(),
+			milesFromUni: Joi.number().required().min(0),
+			description: Joi.string().required(),
+			extraCoolThings: Joi.string().required(),
+			images: Joi.string()
+		}).required()
+	})
+	const { error } = hometownSchema.validate(req.body);
+	if (error) {
+		const errorMessage = error.details.map(el => el.message).join(', '); // make single string message from array of objects
+		throw new AppError(errorMessage, 400);
+	} else {
+		next();
+	}
+}
+
 /**
  * routes
  */
@@ -51,7 +71,7 @@ app.get('/hometowns/new', (req, res) => {
 	res.render('hometowns/new');
 });
 
-app.post('/hometowns', wrapAsync(async (req, res) => {
+app.post('/hometowns', validateHometown, wrapAsync(async (req, res) => {
 	const hometown = new Hometown(req.body.hometown);
 	await hometown.save();
 	res.redirect(`/hometowns/${hometown._id}`);
@@ -67,7 +87,7 @@ app.get('/hometowns/:id/edit', wrapAsync(async (req, res) => {
 	res.render('hometowns/edit', { hometown });
 }));
 
-app.put('/hometowns/:id', wrapAsync(async (req, res) => {
+app.put('/hometowns/:id', validateHometown, wrapAsync(async (req, res) => {
 	const { id } = req.params;
 	const hometown = await Hometown.findByIdAndUpdate(id, { ...req.body.hometown }, { runValidators: true, new: true });
 	res.redirect(`/hometowns/${hometown._id}`);
@@ -79,10 +99,15 @@ app.delete('/hometowns/:id', wrapAsync(async (req, res) => {
 	res.redirect('/hometowns');
 }));
 
+app.all('*', (req, res, next) => {
+	next(new AppError('Page not found', 404));
+});
+
 // error handling middleware
 app.use((err, req, res, next) => {
-	const { status = 500, message = 'Something went wrong' } = err;
-	res.status(status).send(message);
+	const { status = 500 } = err;
+	if (!err.message) err.message = 'Sorry, something went wrong!';
+	res.status(status).render('error', { err });
 });
 
 
